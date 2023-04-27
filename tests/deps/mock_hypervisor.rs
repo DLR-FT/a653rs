1eb3830831598332fcf99e842851498aaf877196
Durak, Umut <umut.durak@dlr.de>
2023-04-27 10:10:44 +0200

    The initial attempt for integration tests

---
+++ tests/deps/mock_hypervisor.rs
@@ -0,0 +1,207 @@
+use a653rs::bindings::*;
+
+pub struct Mock_Hypervisor;
+
+impl ApexPartitionP4 for Mock_Hypervisor {
+    fn get_partition_status<L: Locked>() -> ApexPartitionStatus {
+        let status = ApexPartitionStatus {
+            period: (10),
+            duration: (10),
+            identifier: (0),
+            lock_level: (0),
+            operating_mode: (OperatingMode::ColdStart),
+            start_condition: (StartCondition::NormalStart),
+            num_assigned_cores: (1),
+        };
+        return status;
+        //todo!()
+    }
+
+    fn set_partition_mode<L: Locked>(
+        _operating_mode: OperatingMode,
+    ) -> Result<(), ErrorReturnCode> {
+        todo!()
+    }
+}
+
+impl ApexQueuingPortP1 for Mock_Hypervisor {
+    fn get_queuing_port_id<L: Locked>(
+        _queuing_port_name: QueuingPortName,
+    ) -> Result<QueuingPortId, ErrorReturnCode> {
+        todo!()
+    }
+}
+
+impl ApexQueuingPortP4 for Mock_Hypervisor {
+    fn create_queuing_port<L: Locked>(
+        _queuing_port_name: QueuingPortName,
+        _max_message_size: MessageSize,
+        _max_nb_message: MessageRange,
+        _port_direction: PortDirection,
+        _queuing_discipline: QueuingDiscipline,
+    ) -> Result<QueuingPortId, ErrorReturnCode> {
+        todo!()
+    }
+
+    fn send_queuing_message<L: Locked>(
+        _queuing_port_id: QueuingPortId,
+        _message: &[ApexByte],
+        _time_out: ApexSystemTime,
+    ) -> Result<(), ErrorReturnCode> {
+        todo!()
+    }
+
+    unsafe fn receive_queuing_message<L: Locked>(
+        _queuing_port_id: QueuingPortId,
+        _time_out: ApexSystemTime,
+        _message: &mut [ApexByte],
+    ) -> Result<MessageSize, ErrorReturnCode> {
+        todo!()
+    }
+
+    fn get_queuing_port_status<L: Locked>(
+        _queuing_port_id: QueuingPortId,
+    ) -> Result<QueuingPortStatus, ErrorReturnCode> {
+        todo!()
+    }
+
+    fn clear_queuing_port<L: Locked>(
+        _queuing_port_id: QueuingPortId,
+    ) -> Result<(), ErrorReturnCode> {
+        todo!()
+    }
+}
+
+impl ApexSamplingPortP4 for Mock_Hypervisor {
+    fn create_sampling_port<L: Locked>(
+        _sampling_port_name: SamplingPortName,
+        _max_message_size: MessageSize,
+        _port_direction: PortDirection,
+        _refresh_period: ApexSystemTime,
+    ) -> Result<SamplingPortId, ErrorReturnCode> {
+        todo!()
+    }
+
+    fn write_sampling_message<L: Locked>(
+        _sampling_port_id: SamplingPortId,
+        _message: &[ApexByte],
+    ) -> Result<(), ErrorReturnCode> {
+        todo!()
+    }
+
+    unsafe fn read_sampling_message<L: Locked>(
+        _sampling_port_id: SamplingPortId,
+        _message: &mut [ApexByte],
+    ) -> Result<(Validity, MessageSize), ErrorReturnCode> {
+        todo!()
+    }
+}
+
+impl ApexSamplingPortP1 for Mock_Hypervisor {
+    fn get_sampling_port_id<L: Locked>(
+        _sampling_port_name: SamplingPortName,
+    ) -> Result<SamplingPortId, ErrorReturnCode> {
+        todo!()
+    }
+
+    fn get_sampling_port_status<L: Locked>(
+        _sampling_port_id: SamplingPortId,
+    ) -> Result<ApexSamplingPortStatus, ErrorReturnCode> {
+        todo!()
+    }
+}
+
+impl ApexProcessP4 for Mock_Hypervisor {
+    fn create_process<L: Locked>(
+        _attributes: &ApexProcessAttribute,
+    ) -> Result<ProcessId, ErrorReturnCode> {
+        todo!()
+    }
+
+    fn start<L: Locked>(_process_id: ProcessId) -> Result<(), ErrorReturnCode> {
+        todo!()
+    }
+}
+
+impl ApexProcessP1 for Mock_Hypervisor {
+    fn set_priority<L: Locked>(
+        _process_id: ProcessId,
+        _priority: Priority,
+    ) -> Result<(), ErrorReturnCode> {
+        todo!()
+    }
+
+    fn suspend_self<L: Locked>(_time_out: ApexSystemTime) -> Result<(), ErrorReturnCode> {
+        todo!()
+    }
+
+    fn suspend<L: Locked>(_process_id: ProcessId) -> Result<(), ErrorReturnCode> {
+        todo!()
+    }
+
+    fn resume<L: Locked>(_process_id: ProcessId) -> Result<(), ErrorReturnCode> {
+        todo!()
+    }
+
+    fn stop_self<L: Locked>() {
+        todo!()
+    }
+
+    fn stop<L: Locked>(_process_id: ProcessId) -> Result<(), ErrorReturnCode> {
+        todo!()
+    }
+
+    fn delayed_start<L: Locked>(
+        _process_id: ProcessId,
+        _delay_time: ApexSystemTime,
+    ) -> Result<(), ErrorReturnCode> {
+        todo!()
+    }
+
+    fn lock_preemption<L: Locked>() -> Result<LockLevel, ErrorReturnCode> {
+        todo!()
+    }
+
+    fn unlock_preemption<L: Locked>() -> Result<LockLevel, ErrorReturnCode> {
+        todo!()
+    }
+
+    fn get_my_id<L: Locked>() -> Result<ProcessId, ErrorReturnCode> {
+        todo!()
+    }
+
+    fn get_process_id<L: Locked>(_process_name: ProcessName) -> Result<ProcessId, ErrorReturnCode> {
+        todo!()
+    }
+
+    fn get_process_status<L: Locked>(
+        _process_id: ProcessId,
+    ) -> Result<ApexProcessStatus, ErrorReturnCode> {
+        todo!()
+    }
+
+    fn initialize_process_core_affinity<L: Locked>(
+        _process_id: ProcessId,
+        _processor_core_id: ProcessorCoreId,
+    ) -> Result<(), ErrorReturnCode> {
+        todo!()
+    }
+
+    fn get_my_processor_core_id<L: Locked>() -> ProcessorCoreId {
+        todo!()
+    }
+
+    fn get_my_index<L: Locked>() -> Result<ProcessIndex, ErrorReturnCode> {
+        todo!()
+    }
+}
+
+impl ApexTimeP4 for Mock_Hypervisor {
+    fn periodic_wait() -> Result<(), ErrorReturnCode> {
+        todo!()
+    }
+
+    fn get_time() -> ApexSystemTime {
+        todo!()
+    }
+}
---
+++ tests/integration_tests.rs
@@ -0,0 +1,59 @@
+use a653rs::prelude::PartitionExt;
+use a653rs_macros::partition;
+
+mod deps;
+
+#[partition(crate::deps::mock_hypervisor::Mock_Hypervisor)]
+mod test_partition {
+    #[sampling_out(name = "Ch1", msg_size = "10KB")]
+    struct Channel1;
+
+    #[sampling_in(refresh_period = "500ms")]
+    #[sampling_in(msg_size = "25KB")]
+    struct ChannelTwo;
+
+    #[queuing_out(msg_count = 20, msg_size = "12KB", discipline = "FIFO")]
+    struct Channel3;
+
+    #[start(cold)]
+    fn cold_start(ctx: start::Context) {
+        warm_start(ctx);
+    }
+
+    #[start(warm)]
+    fn warm_start(mut ctx: start::Context) {
+        ctx.create_aperiodic2().unwrap().start().unwrap();
+        ctx.create_periodic3().unwrap().start().unwrap();
+        ctx.create_channel_1().unwrap();
+        ctx.create_channel_two().unwrap();
+
+        // Maybe we do not always want to initialize channel3
+        // ctx.create_channel_3().unwrap();
+    }
+
+    #[aperiodic(
+        name = "ap2",
+        time_capacity = "Infinite",
+        stack_size = "10KB",
+        base_priority = 1,
+        deadline = "Soft"
+    )]
+    fn aperiodic2(ctx: aperiodic2::Context) {
+        ctx.get_time();
+    }
+
+    #[periodic(
+        period = "10ms",
+        time_capacity = "Infinite",
+        stack_size = "10KB",
+        base_priority = 1,
+        deadline = "Hard"
+    )]
+    fn periodic3(_ctx: periodic3::Context) {}
+}
+
+#[test]
+#[should_panic]
+fn test_partitionrun() {
+    test_partition::Partition.run();
+}
