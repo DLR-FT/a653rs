7c89f9623249001e65930644d1939ba6a6fab4c4
Sven Friedrich <sven@autumnal.de>
2023-07-17 13:04:22 +0200

    allow both start flags (cold, warm) for single function

--- macros/src/parse/start.rs
+++ macros/src/parse/start.rs
@@ -29,10 +29,8 @@ struct StartFlags {
     cold: Flag,
 }
 
-impl TryFrom<StartFlags> for Option<StartType> {
-    type Error = syn::Error;
-
-    fn try_from(value: StartFlags) -> Result<Self, Self::Error> {
+impl From<StartFlags> for Vec<StartType> {
+    fn from(value: StartFlags) -> Self {
         let mut flags = vec![];
         if value.warm.is_present() {
             flags.push(StartType::Warm(value.warm.span()))
@@ -40,21 +38,7 @@ impl TryFrom<StartFlags> for Option<StartType> {
         if value.cold.is_present() {
             flags.push(StartType::Cold(value.cold.span()))
         }
-        match flags.len() {
-            0 => Ok(None),
-            1 => Ok(Some(flags[0])),
-            _ => {
-                let mut flags = flags.iter();
-                let mut err = syn::Error::new(
-                    *flags.next().unwrap().span(),
-                    "Multiple start flags attached to same function.",
-                );
-                for (i, flag) in flags.enumerate() {
-                    err.combine(syn::Error::new(*flag.span(), format!("{}th flag", i + 2)))
-                }
-                Err(err)
-            }
-        }
+        flags
     }
 }
 
@@ -97,28 +81,23 @@ impl Start {
             _ => None,
         }) {
             let start = StartFlags::from_attributes(&item.attrs)?;
-            let start: Option<StartType> = start.try_into()?;
-            let start = if let Some(start) = start {
-                start
-            } else {
-                continue;
-            };
+            let starts: Vec<StartType> = start.into();
+            for start in starts {
+                // Remove start attributes from item
+                remove_attributes("start", &mut item.attrs)?;
 
-            // Remove start attributes from item //
-            remove_attributes("start", &mut item.attrs)?;
-            // Remove start attributes from item //
-
-            let leftover = match start {
-                StartType::Warm(_) => warm.replace(item.clone()),
-                StartType::Cold(_) => cold.replace(item.clone()),
-            };
-            if let Some(leftover) = leftover {
-                let mut err = syn::Error::new(
-                    item.span(),
-                    format!("{}Start already defined", start.as_ref()),
-                );
-                err.combine(syn::Error::new(leftover.span(), "First definition here"));
-                return Err(err);
+                let leftover = match start {
+                    StartType::Warm(_) => warm.replace(item.clone()),
+                    StartType::Cold(_) => cold.replace(item.clone()),
+                };
+                if let Some(leftover) = leftover {
+                    let mut err = syn::Error::new(
+                        item.span(),
+                        format!("{}Start already defined", start.as_ref()),
+                    );
+                    err.combine(syn::Error::new(leftover.span(), "First definition here"));
+                    return Err(err);
+                }
             }
         }
         Start {
